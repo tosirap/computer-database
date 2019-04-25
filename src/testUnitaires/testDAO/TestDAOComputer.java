d40c7e6c6d1c3c329c14b0862377a8d6e017a4bf
tosirap <parisotpaul@live.fr>
2019-04-25 10:38:55 +0200

    Tests unitaires sur DAOComputer, et push avant mavenisation du projet

--- src/testUnitaires/testDAO/TestDAOComputer.java
+++ src/testUnitaires/testDAO/TestDAOComputer.java
@@ -13,30 +13,58 @@ import com.excilys.cdb.model.Computer;
 
 public class TestDAOComputer {
 
-//private Computer computer;
+private Computer computer;
 	DAOComputer daoComp;
 
 	@Before
 	public void setUp() throws Exception {
-		// computer = new Computer(2,"bloblo", new Date("2017-11-11"), new
-		// Date("2017-11-11"), 4);
+		computer = new Computer("2","bloblo", "2017-11-11", "2017-11-11", "4", "Netronics");
 		daoComp = DAOComputer.getInstance();
-		daoComputerInsert();
+		daoComputerFindOneCorrecte();
+		daoComputerFindOneInCorrecte1();
+		daoComputerFindOneInCorrecte2();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		//computer = null;
+		computer = null;
 
 	}
 
 	@Test
-	public void daoComputerInsert() {
+	public void daoComputerFindOneCorrecte() {
 		Computer actual = daoComp.find(12);
-		System.out.println(actual.toString());
 		Computer expected = new Computer("12", "Apple III", "1980-05-01", "1984-04-01", "1",
 				"Apple Inc.");
 		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void daoComputerFindOneInCorrecte1() {
+		Computer computer = daoComp.find(100999887);
+		assertTrue(computer.getId() == -1);
+	}
+	
+	@Test
+	public void daoComputerFindOneInCorrecte2() {
+		Computer computer = daoComp.find(100999887);
+		assertTrue(computer.getId() == -1);
+	}
+	
+	@Test
+	public void daoComputerCreateCorrecte() {
+		int nbComputerBefore = daoComp.findAll().size();
+		boolean b = daoComp.create(computer);
+		int nbComputerAfter = daoComp.findAll().size();
+		assertTrue((nbComputerAfter  - nbComputerBefore == 1)&&b);		
+	}
+	
+	
+	
+	
+	@Test
+	public void daoComputerUpdateCorrecte() {
+		
 	}
 
 }
