393a114e64719fc9efff7f1d8546dc5add085b5a
tosirap <parisotpaul@live.fr>
2019-05-09 17:57:40 +0200

    To do terminer validators

---
+++ src/main/java/com/excilys/cdb/validator/ValidatorCompanyUIWeb.java
@@ -0,0 +1,15 @@
+package com.excilys.cdb.validator;
+
+import com.excilys.cdb.transfert.DTOCompany;
+
+public class ValidatorCompanyUIWeb {
+
+		public ValidatorCompanyUIWeb() {
+			
+		}
+		
+		public boolean correct(DTOCompany dtoCompany) {
+			
+			return true;
+		}
+}
---
+++ src/main/java/com/excilys/cdb/validator/ValidatorComputerUIweb.java
@@ -0,0 +1,15 @@
+package com.excilys.cdb.validator;
+
+import com.excilys.cdb.transfert.DTOCompany;
+
+public class ValidatorComputerUIweb {
+
+	public ValidatorComputerUIweb() {
+		
+	}
+	
+	public boolean correct(DTOCompany dtoComputer) {
+		
+		return true;
+	}
+}
