bd1664681931d8c11deff74bb625a93749e559dc
tosirap <parisotpaul@live.fr>
2019-05-22 16:43:29 +0200

    Spring mvc editComputer, mais pas testé

--- src/main/java/com/excilys/cdb/servletServer/EditComputerServlet.java
+++ src/main/java/com/excilys/cdb/servletServer/EditComputerServlet.java
@@ -63,8 +63,6 @@ public class EditComputerServlet {
 			@RequestParam(value = "company", required = false) String companyId, Model model) {
 
 		DTOComputer dtoComputer = new DTOComputer(id, name, introduced, discontinued, companyId, "");
-		System.out.println(dtoComputer);
-		System.out.println(mappeurComputer.DTOToComputer(dtoComputer));
 		if (serviceComputer.update(mappeurComputer.DTOToComputer(dtoComputer))) {
 			model.addAttribute("message", message);
 		} else {
@@ -73,59 +71,3 @@ public class EditComputerServlet {
 		return "editComputer";
 	}
 }
-/*
- * @Override public void doGet(HttpServletRequest request, HttpServletResponse
- * response) throws ServletException, IOException {
- * 
- * request.setAttribute("id", request.getParameter("id"));
- * request.setAttribute("name", request.getParameter("name"));
- * request.setAttribute("intro", request.getParameter("intro"));
- * request.setAttribute("discon", request.getParameter("discon"));
- * request.setAttribute("company", request.getParameter("company"));
- * 
- * ArrayList<DTOCompany> alCompany =
- * mappeurCompany.companyToDTO(serviceCompany.listAllElements());
- * request.setAttribute("listCompany", alCompany);
- * 
- * try { RequestDispatcher rd =
- * request.getRequestDispatcher("WEB-INF/views/editComputer.jsp");
- * rd.forward(request, response); } catch (Exception e) {
- * logger.info(e.getMessage()); } }
- * 
- * @Override public void doPost(HttpServletRequest request, HttpServletResponse
- * response) throws ServletException, IOException {
- * 
- * String id = ""; if (request.getParameter("id") != null) { id =
- * request.getParameter("id"); } String name = ""; if
- * (request.getParameter("name") != null) { name = request.getParameter("name");
- * }
- * 
- * String introduced = ""; if (request.getParameter("intro") != null &&
- * !request.getParameter("intro").equals("")) { introduced =
- * String.valueOf(request.getParameter("intro")); }
- * 
- * String discontinued = ""; if (request.getParameter("discon") != null &&
- * !request.getParameter("discon").equals("")) { discontinued =
- * String.valueOf(request.getParameter("discon")); }
- * 
- * String companyId = ""; if (request.getParameter("company") != null) {
- * companyId = request.getParameter("company"); } DTOComputer dtoComputer = new
- * DTOComputer(id, name, introduced, discontinued, companyId, ""); boolean b =
- * false; String messageErreur = "Erreur /!\\"; if
- * (validatorComputerUIweb.testSiCorrect(dtoComputer)) { // appel au validator b
- * = serviceComputer.update(mappeurComputer.DTOToComputer(dtoComputer)); if (b)
- * { request.setAttribute("message", "Update effectuée"); messageErreur = null;
- * } } request.setAttribute("messageErreur", messageErreur);
- * ArrayList<DTOCompany> alCompany =
- * mappeurCompany.companyToDTO(serviceCompany.listAllElements());
- * request.setAttribute("listCompany", alCompany); request.setAttribute("id",
- * request.getParameter("id")); request.setAttribute("name",
- * request.getParameter("name")); request.setAttribute("intro",
- * request.getParameter("intro")); request.setAttribute("discon",
- * request.getParameter("discon")); request.setAttribute("company",
- * request.getParameter("company")); try { RequestDispatcher rd =
- * request.getRequestDispatcher("WEB-INF/views/editComputer.jsp");
- * rd.forward(request, response);
- * 
- * } catch (Exception e) { logger.info(e.getMessage()); } } }
- */
