7e26bb80902d5f0ca1a050149e85e51962b78d36
tosirap <parisotpaul@live.fr>
2019-05-29 11:17:47 +0200

    push avant la hess

--- src/main/java/com/excilys/cdb/configSpring/AppConfig.java
+++ src/main/java/com/excilys/cdb/configSpring/AppConfig.java
@@ -5,29 +5,26 @@ import java.util.TimeZone;
 
 import javax.sql.DataSource;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
+@EnableTransactionManagement
 @ComponentScan({ "com.excilys.cdb.dao", "com.excilys.cdb.transfert", "com.excilys.cdb.service",
 		"com.excilys.cdb.validator" })
 public class AppConfig {
 
-	@Bean
+/*	@Bean
 	public DataSource dataSource() {
-		
+
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		return new HikariDataSource(new HikariConfig("/hikary.properties"));
 	}
@@ -37,10 +34,6 @@ public class AppConfig {
 //		return new HikariConfig("/hikary.properties");
 //	}
 
-	@Bean
-	public PlatformTransactionManager transactionManager(DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
-	}
 
 	@Bean
 	public MessageSource messageSource() {
@@ -49,32 +42,77 @@ public class AppConfig {
 		return bundleMessage;
 	}
 
-	@Bean
-	@Autowired
-	public LocalSessionFactoryBean sessionFactory(DataSource ds) {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setDataSource(ds);
-		sessionFactory.setPackagesToScan(new String[] { "com.excilys.cdb.model" });
-		sessionFactory.setHibernateProperties(this.hibernateProperties());
-		return sessionFactory;
-	}
 	
+	
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource ds) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(ds);
+        System.out.println("ici");
+        sessionFactory.setPackagesToScan(new String[] {"com.excilys.cdb.model" });
+        System.out.println("ici2");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        System.out.println("ici3");
+        return sessionFactory;
+    }
+	
+	
+
 	@Bean
-	@Autowired
 	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
 		HibernateTransactionManager txManager = new HibernateTransactionManager();
 		txManager.setSessionFactory(sessionFactory);
 		return txManager;
 	}
 	
-	private Properties hibernateProperties() {
-		return new Properties() {
-			private static final long serialVersionUID = 27052019L;
-			{
-				setProperty("hibernate.hbm2ddl.auto", "validate");
-				setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-				setProperty("hibernate.globally_quoted_identifiers", "true");
-			}
-		};
-}
+	
+	 private final Properties hibernateProperties() {
+	        Properties hibernateProperties = new Properties();
+	        hibernateProperties.setProperty(
+	          "hibernate.hbm2ddl.auto", "create-drop");
+	        hibernateProperties.setProperty(
+	          "hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+	        return hibernateProperties;
+	    }
+	 
+	 
+	 */
+	 
+	 
+	    @Bean
+	    public LocalSessionFactoryBean sessionFactory() {
+	        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+	        sessionFactory.setDataSource(dataSource());
+	        sessionFactory.setPackagesToScan(
+	          "com.excilys.cdb.model" );
+	        sessionFactory.setHibernateProperties(hibernateProperties());
+	 
+	        return sessionFactory;
+	    }
+	 
+	    @Bean
+	    public DataSource dataSource() {
+	    	TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+			return new HikariDataSource(new HikariConfig("/hikary.properties"));
+	    }
+	 
+	    @Bean
+	    public PlatformTransactionManager hibernateTransactionManager() {
+	        HibernateTransactionManager transactionManager
+	          = new HibernateTransactionManager();
+	        transactionManager.setSessionFactory(sessionFactory().getObject());
+	        return transactionManager;
+	    }
+	 
+	    private final Properties hibernateProperties() {
+	        Properties hibernateProperties = new Properties();
+	        hibernateProperties.setProperty(
+	          "hibernate.hbm2ddl.auto", "validate");
+	        hibernateProperties.setProperty(
+	          "hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+	 
+	        return hibernateProperties;
+	    }
+	 
+	 
 }
