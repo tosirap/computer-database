842afa005ec75eba0dbbbc3209a6613031617a7d
tosirap <parisotpaul@live.fr>
2019-06-06 18:01:42 +0200

    Spring secu , on continue

--- webapp/src/main/java/com/excilys/cdb/configSpring/SpringSecurityConfig.java
+++ webapp/src/main/java/com/excilys/cdb/configSpring/SpringSecurityConfig.java
@@ -2,10 +2,12 @@ package com.excilys.cdb.configSpring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,15 +15,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private final UserDetailsService userService;
+
+	public SpringSecurityConfig(UserDetailsService userService) {
+		super();
+		this.userService = userService;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/fonts/**").permitAll()
 
+				.antMatchers(HttpMethod.GET, "/", "/dashboard", "/error").permitAll()
+				.antMatchers(HttpMethod.GET, "/addComputer").hasRole("USER")
+				.antMatchers(HttpMethod.POST, "/addComputer").hasRole("USER")
+				.antMatchers(HttpMethod.GET, "/editComputer").hasRole("USER")
+				.antMatchers(HttpMethod.POST, "/editComputer").hasRole("USER")
+				.antMatchers(HttpMethod.POST, "/delete").hasRole("USER")
+				
+				.anyRequest().hasRole("ADMIN").
+				and().formLogin().loginPage("/login").permitAll()
+				.loginProcessingUrl("/login").usernameParameter("username").passwordParameter("password").and().logout()
+				.logoutUrl("/logout").permitAll().logoutSuccessUrl("/computers").and().csrf().disable();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(this.passwordEncoder()).withUser("user")
-				.password(this.passwordEncoder().encode("pass")).roles("USER");
+		auth.userDetailsService(userService);
 	}
 
 	@Bean
---
+++ webapp/src/main/java/com/excilys/cdb/configSpring/WebSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.excilys.cdb.configSpring;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
